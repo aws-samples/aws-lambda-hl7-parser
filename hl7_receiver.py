22f8e8962e669ca639e0bbc596c42c63ae282d9d
Elden Jahnke <94935251+ejahnke@users.noreply.github.com>
2021-12-15 23:39:50 -0800

    Update hl7_receiver.py

--- hl7_receiver.py
+++ hl7_receiver.py
@@ -1,33 +1,49 @@
+#Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
+
+"""
+Purpose
+
+When configured as an AWS SQS destination, this Python AWS Lambda function pops 
+incoming HL7 messages from an AWS SQS FIFO queue and parses them into a JSON object. 
+It parses Segments, Elements and Fields. Component, sub components and repetition 
+functionality is not currently supported.
+
+"""
+
 import os
 import json
 import boto3
-import hl7apy
-from hl7apy import parser
 from hl7apy.parser import parse_message
-from hl7apy.parser import parse_field
-from hl7apy.core import Group, Segment
+from hl7apy.core import Segment
+from botocore.exceptions import ClientError
 
-region = os.environ['region']
-account = os.environ['account']
+#email address to be used to send notifications
+sender = os.environ['sender']
 
 #create boto3 clients to interact with other AWS services
-cm_client = boto3.client(service_name='comprehendmedical', region_name=region)
+
+cm_client = boto3.client(service_name='comprehendmedical')
 client = boto3.client('lambda')
-sqs_client = boto3.client("sqs", region_name=region)
-sns_client = boto3.client('sns')
+sqs_client = boto3.client("sqs")
+ses_client = boto3.client('ses')
 
 #main function invoked by lambda
 def lambda_handler(event, context):
 
+    principal = event.get("requestContext", {}).get("identity", {}).get("userArn", "unknown")
+
     #HL7 segments that will be parsed into JSON
     scopedSegments = ['MSH','EVN','PID','PV1','PD1','OBX']
     
+    charset = "UTF-8"
+    
+    #parse incoming HL7 message and create a JSON object
     for record in event['Records']:
         
         msg = parse_message(record["body"].replace('\n', '\r').replace('^^','^""^'))
-        
-        data='{'
-        
+
+        parsedJsonMessage = {}
         for segment in msg.children:
             if isinstance(segment, Segment):
                 if segment.name in scopedSegments:
@@ -35,94 +51,149 @@ def lambda_handler(event, context):
                             index=0
                             for element in field.children:
                                     if element.value != '""':
-                                        data=data + '"' +   field.name+'_'+str(index) + '":"' +  element.value.replace('\\F\\','|').replace('\\S\\','^').replace('\\R\\','~').replace('\\E\\','').replace('\\T\\','&') + '",'
+                                        parsedJsonMessage[field.name+'_'+str(index)] = element.value.replace('\\F\\','|').replace('\\S\\','^').replace('\\R\\','~').replace('\\E\\','').replace('\\T\\','&')
                                     index=index+1
         
-        data = data[:-1] + ', "medications":[], "conditions": [] }'
+        parsedJsonMessage['medications'] = []
+        parsedJsonMessage['conditions'] = []
         
-        data2=json.loads(data)
+        #instantiate data needed for notifications
+        msgId               = parsedJsonMessage.get("MSH_10_0","")
+        patLastName         = parsedJsonMessage.get("PID_5_0","")
+        patFirstName        = parsedJsonMessage.get("PID_5_1","")
+        eventType           = parsedJsonMessage.get("EVN_1_0","")
+        facility            = parsedJsonMessage.get("MSH_6_0","")
+        providerLastName    = parsedJsonMessage.get("PD1_4_1","")
+        providerFirstName   = parsedJsonMessage.get("PD1_4_2","")
+        providerEmail       = parsedJsonMessage.get("PD1_4_0","")
+        patAddressLine      = parsedJsonMessage.get("PID_11_0","")
+        patAddressCity      = parsedJsonMessage.get("PID_11_2","")
+        patAddressProvince  = parsedJsonMessage.get("PID_11_3","")
+        patAddressPostal    = parsedJsonMessage.get("PID_11_4","")
+        admissionNotes      = parsedJsonMessage.get("OBX_5_0","")
+        
+        #enrinch parsedJsonMessage JSON document with geo location information based on patient address from Amazon Location Service
+        addressEnrichedJson = discoverAddress(
+           patAddressLine,
+           patAddressCity,
+           patAddressProvince,
+           patAddressPostal,
+           parsedJsonMessage
+        )
+               
+        #enrich addressEnrichedJson JSON document with Conditions and Medications from Amazon Comprehend Medical       
+        updatedJsonMessage = discoverConditionsMedications(
+            admissionNotes,
+            addressEnrichedJson
+        )    
+        
+        medicalConditions   =""
+        medications         =""
+        
+        #prepare conditions and medications strings for email notification
+        medicalConditions = ", ".join(updatedJsonMessage.get("conditions",[]))
+        medications = ", ".join(updatedJsonMessage.get("medications",[]))
+        
+        #send outbound email notification via Amazon SES
+        sendEmailNotification(
+            patLastName,
+            patFirstName,
+            eventType,
+            facility,
+            providerLastName,
+            providerFirstName,
+            providerEmail,
+            medicalConditions,
+            medications,
+            charset)
+        
+    maskedEmail = maskEmail(providerEmail)
+    print(f"User {principal} issued a notification request for the following Hl7 message {msgId} and the following destination {maskedEmail}")
+        
+    return {
+        'statusCode': 200,
+        'body': 'OK'
+    }
+    
+    
+def sendEmailNotification(patLastName, patFirstName,eventType,facility,providerLastName,providerFirstName,providerEmail,conditions,medications,charset):
+    
+    #function to send outbound email notifications via Amazon SES
+    
+    message="""Patient Name: %s, %s
+Event: %s
+Facility: %s
+Family Doctor: %s, %s
+Medical Conditions on encouter: %s
+Medications: %s""" % (patLastName,patFirstName,eventType,facility,providerLastName,providerFirstName,conditions,medications)
+    recipient = providerEmail
+    subject = "New %s notification for %s %s" % (eventType,patFirstName,patLastName)
+        
+    try:
+        #Provide the contents of the email.
+        #recipient email address needs to be validated against MDM solution for authenticity and validity
+        
+        dest = {'ToAddresses': [recipient]}
+        msg = {'Body': { 'Text': {'Charset': charset,'Data': message}},'Subject': {'Charset': charset,'Data': subject}}
+            
+        ses_client.send_email(Destination=dest,Message=msg,Source=sender)
+        
+    except Exception as err:
+        print('Error sending provider notification: ' + str(err) )
+        
+        
+def discoverConditionsMedications(admissionNotes,jsonDoc):
+    #invoke Amazon Comnprehend Medical with admission notes in OBX5
+    #this is an optional step but can add value to discover and parse admission notes
+    #in the event of an issue, the idea is to keep going with the notification
 
-       #invoke Amazon Location Services and gather geo coordinates based on patient address
-       #this is an optional step but can add value to geographically locate patients
+    medIndx=0
+    conIndx=0
+    try:
+        cm_result = cm_client.detect_entities(Text=admissionNotes)
+        entities = cm_result['Entities']
         
-        geoEvent={}
+        for entity in entities:
+            if entity["Category"] == 'MEDICATION':
+                jsonDoc["medications"].append(entity["Text"])
+                      
+            if entity["Category"] == 'MEDICAL_CONDITION':
+                jsonDoc["conditions"].append(entity["Text"])
+    except Exception as err:
+        print("Error parsing comprehend response: " + str(err))
+    finally:
+        return jsonDoc
         
-        try:
-            geoEvent = {
-                "address_line": data2["PID_11_0"],
-                "municipality_name": data2["PID_11_2"],
-                "state_code": data2["PID_11_3"],
-                "post_code": data2["PID_11_4"]
-            }
-        except:
-            print("Issues parsing the address")
+        
+def discoverAddress(patAddressLine,patAddressCity,patAddressProvince,patAddressPostal,jsonDoc):
+    #invoke Amazon Location Services and gather geo coordinates based on patient address
+    #this is an optional step but can add value to geographically locate patients
+    #in the event of an issue, the idea is to keep going with the notification
+        
+    geoEvent={}
+    
+    try:
+        geoEvent = {
+            "address_line": patAddressLine,
+            "municipality_name": patAddressCity,
+            "state_code": patAddressProvince,
+            "post_code": patAddressPostal
+        }
         
         geo_response = client.invoke(
-            FunctionName='arn:aws:lambda:'+region+':'+account+':function:get-geo-location',
+            FunctionName='function:get-geo-location',
             InvocationType='RequestResponse',
             Payload=json.dumps(geoEvent)
         )
         
-        resp = geo_response["Payload"].read().decode().replace('"','')
+        resp = json.loads(geo_response["Payload"].read().decode())
+        jsonDoc.update({"location_geo": str(resp.get('Latitude',"0.0")) + "," + str(resp.get('Longitude',"0.0"))})
+    except Exception as err:
+        print("Issues parsing the address: " + str(err))
+    finally:
+        return jsonDoc
         
-        geo_location = {"location_geo": resp}
-        
-        data2.update(geo_location)
-        
-               
-        #invoke Amazon Comnprehend Medical with admission notes in OBX5
-        #this is an optional step but can add value to discover and parse admission notes
-
-        medIndx=0
-        conIndx=0
-        try:
-            cm_result = cm_client.detect_entities(Text= data2["OBX_5_0"])
-            entities = cm_result['Entities'];
-            for entity in entities:
-                #print('Entity', entity["Category"], entity["Text"])
-                if entity["Category"] == 'MEDICATION':
-                    #print(entity["Text"])
-                    for attribute in entity["Attributes"]:
-                        if attribute["Type"] in ['DOSAGE']:
-                            #print(entity["Text"],attribute["Text"])
-                            med = {entity["Category"]:entity["Text"] + "("+attribute["Text"]+")"}
-                            data2["medications"].append(med)
-                            
-                   
-                if entity["Category"] == 'MEDICAL_CONDITION':
-                    cond = {entity["Category"]:entity["Text"]}
-                    data2["conditions"].append(cond)
-        except:
-            print("Error parsing comprehend response")
-            
-                
-        medicalConditions=""
-        medications=""
-        
-        for key in data2["conditions"]:
-            #print(key)
-            medicalConditions += key["MEDICAL_CONDITION"] + ", "
-            
-        for key in data2["medications"]:
-            #print(key)
-            medications += key["MEDICATION"] + ", "
-            
-        
-        message="""Patient Name: %s, %s
-Event: %s
-Facility: %s
-Family Doctor: %s, %s
-Department Contact: %s
-Medical Conditions on encouter: %s
-Medications: %s""" % (data2["PID_5_0"],data2["PID_5_1"],data2["EVN_1_0"],data2["MSH_6_0"],data2["PD1_4_1"],data2["PD1_4_2"],"(416)555-1234",medicalConditions[:-2],medications[:-2])
-        
-        response = sns_client.publish(
-            TargetArn="arn:aws:sns:"+region+":"+account+":" + data2["PD1_4_0"] +"-provider-notifications",
-            Message=message
-        )
-        print(response)
-        
-    return {
-        'statusCode': 200,
-        'body': json.dumps(data2)
-    }
+def maskEmail(providerEmail):
+    lo = providerEmail.find('@')
+    if lo>0:
+        return providerEmail[0]+'********'+providerEmail[lo-1:]
